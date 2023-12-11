c94fb3a33662d308070be40a1827c5ef0bd84c2a
cmoreiraa <cmoreiraalen@danielcastelao.org>
2023-12-11 11:27:22 +0100

    END

--- README.md
+++ README.md
@@ -42,5 +42,93 @@ cualquier instalación de WP.
 
     Por lo que se ve el profe quiere seguir trabajando en este proyecto así que a seguir. 
 
-# Tengo problema con esta tarea y no sé porqué no funciona como debería, lo iré actualizando a medida que vaya avanzando en el proyecto. Intentaré que me ayude algun compañero de clase.
+# Tengo problema con esta tarea y no sé porqué no funciona como debería, lo iré actualizando a medida que vaya avanzando en el proyecto. Intentaré que me ayude algun compañero de clase.e
 
+## Actualizacion día 11/12/23
+
+He conseguido que funcione el plugin, asi es como me ha quedado el codigo
+
+```php
+<?php
+/**
+ * @package Palabra Censurada
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Palabra Censurada
+Plugin URI: http://wordpress.org/plugins/Palabra-Censurada/
+Description: Este plugin censura las palabras malsonantes en WordPress.
+Author: Cristian
+Version: 1.0.1
+Author URI: http://ma.tt/
+*/
+
+function inicioPlugin(){
+    createTable();
+    insertData();
+}
+
+/**
+ * Carga tabla wp_dam con las palabras malsonantes
+ */
+function createTable(){
+    global $wpdb;
+    $table_name = 'wp_dam';
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        tabu varchar(255) NOT NULL,
+        eufemismo varchar(255) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+/**
+ * Inserta o actualiza datos en la tabla
+ */
+function insertData() {
+    global $wpdb;
+    $table_name = 'wp_dam';
+    $data = array(
+        array('tabu' => 'puta', 'eufemismo' => 'guapa'),
+        array('tabu' => 'puto', 'eufemismo' => 'guapo'),
+        array('tabu' => 'coño', 'eufemismo' => 'xoxo'),
+        array('tabu' => 'maricon', 'eufemismo' => 'homo'),
+        array('tabu' => 'hijo de', 'eufemismo' => 'tu madre es')
+    );
+
+    foreach ($data as $row) {
+        $existing_row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE tabu = %s", $row['tabu']));
+
+        if ($existing_row) {
+            // Si ya existe, actualiza el eufemismo
+            $wpdb->update($table_name, array('eufemismo' => $row['eufemismo']), array('id' => $existing_row->id));
+        } else {
+            // Si no existe, inserta un nuevo registro
+            $wpdb->insert($table_name, $row);
+        }
+    }
+}
+
+function renym_wordpress_typo_fix($text) {
+    global $wpdb;
+    $table_name = 'wp_dam';
+    $results = $wpdb->get_results( "SELECT * FROM $table_name" );
+
+    foreach ($results as $row) {
+        $text = str_replace($row->tabu, $row->eufemismo, $text);
+    }
+
+    return $text;
+}
+
+add_action('plugins_loaded', 'inicioPlugin');
+add_filter('the_content', 'renym_wordpress_typo_fix');
+
+```
+
+### Por aquí dejamos esta tarea, ha sido un placer trabajar en este proyecto, nos vemos en la próxima.
+
+`Cristian Moreira 2DAM`
--- src/wp-content/plugins/words.php
+++ src/wp-content/plugins/words.php
@@ -1,70 +1,78 @@
 <?php
 /**
- * @package Cristian
+ * @package Palabra Censurada
  * @version 1.0.0
  */
 /*
-Plugin Name: Popi Plugin
-Plugin URI: http://wordpress.org/plugins/cristian-words/
-Description: This plugin is a test for the WordPress plugin development course.
-Author: Popi
-Version: 0.0.1
-Author URI: MiPrima
+Plugin Name: Palabra Censurada
+Plugin URI: http://wordpress.org/plugins/Palabra-Censurada/
+Description: Este plugin censura las palabras malsonantes en WordPress.
+Author: Cristian
+Version: 1.0.1
+Author URI: http://ma.tt/
 */
 
-function inicioPlugin() {
+function inicioPlugin(){
     createTable();
     insertData();
 }
 
 /**
- * Crea la tabla 'dam' en la base de datos
+ * Carga tabla wp_dam con las palabras malsonantes
  */
-function createTable() {
+function createTable(){
     global $wpdb;
-    $table_name = $wpdb->prefix . 'dam';
+    $table_name = 'wp_dam';
     $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE $table_name (
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
-        word varchar(255) NOT NULL,
-        palabrota varchar(255) NOT NULL,
+        tabu varchar(255) NOT NULL,
         eufemismo varchar(255) NOT NULL,
         PRIMARY KEY  (id)
     ) $charset_collate;";
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
 }
 
 /**
- * Inserta datos en la tabla 'dam'
+ * Inserta o actualiza datos en la tabla
  */
 function insertData() {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'dam';
-    $wpdb->insert(
-        $table_name,
-        array(
-            'word' => 'puta',
-            'palabrota' => 'no se dice eso',
-            'eufemismo' => 'no se dice eso'
-            // Agrega más filas según sea necesario
-        )
+    $table_name = 'wp_dam';
+    $data = array(
+        array('tabu' => 'puta', 'eufemismo' => 'guapa'),
+        array('tabu' => 'puto', 'eufemismo' => 'guapo'),
+        array('tabu' => 'coño', 'eufemismo' => 'xoxo'),
+        array('tabu' => 'maricon', 'eufemismo' => 'homo'),
+        array('tabu' => 'hijo de', 'eufemismo' => 'tu madre es')
     );
+
+    foreach ($data as $row) {
+        $existing_row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE tabu = %s", $row['tabu']));
+
+        if ($existing_row) {
+            // Si ya existe, actualiza el eufemismo
+            $wpdb->update($table_name, array('eufemismo' => $row['eufemismo']), array('id' => $existing_row->id));
+        } else {
+            // Si no existe, inserta un nuevo registro
+            $wpdb->insert($table_name, $row);
+        }
+    }
 }
 
-add_action('plugins_loaded', 'inicioPlugin');
-
-/**
- * Filtra el contenido para reemplazar palabras ofensivas
- */
 function renym_wordpress_typo_fix($text) {
     global $wpdb;
-    $table_name = $wpdb->prefix . 'dam';
-    $results = $wpdb->get_results("SELECT * FROM $table_name");
+    $table_name = 'wp_dam';
+    $results = $wpdb->get_results( "SELECT * FROM $table_name" );
+
     foreach ($results as $row) {
-        $text = str_replace($row->word, $row->eufemismo, $text);
+        $text = str_replace($row->tabu, $row->eufemismo, $text);
     }
+
     return $text;
 }
 
+add_action('plugins_loaded', 'inicioPlugin');
 add_filter('the_content', 'renym_wordpress_typo_fix');
+
